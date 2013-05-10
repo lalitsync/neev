dabbb1aacbddebd19607e4dfa926b3de53c4faeb
unknown <lalit.kumawat2synechron.com>
2013-05-11 05:10:40 +0530

    changes for database user and host

--- info.php
+++ info.php
@@ -1,6 +1,6 @@
 <?php
 //$conn = mysql_connect('localhost','root');
-$conn = mysql_connect('tunnel.pagodabox.com:3306','blanch','zDvbHEtG');
+$conn = mysql_connect('72.16.30.209','blanch','zDvbHEtG');
 
 $database =mysql_select_db('neev');
 ?>
