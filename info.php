0b5711cd3152f4ef57e74aa3163b5d64de91d204
unknown <lalit.kumawat2synechron.com>
2013-05-11 04:52:12 +0530

    changes for database 2

--- info.php
+++ info.php
@@ -1,5 +1,6 @@
 <?php
-$conn = mysql_connect('tunnel.pagodabox.com:3306','evelyn','sOupBXHU');
+//$conn = mysql_connect('localhost','root');
+$conn = mysql_connect('tunnel.pagodabox.com:3306','blanch','zDvbHEtG');
 
 $database =mysql_select_db('neev');
 ?>
